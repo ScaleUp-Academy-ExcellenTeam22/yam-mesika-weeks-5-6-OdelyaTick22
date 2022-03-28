0b3e91b14d5231096e2974664a44a4aac9c21ac7
OdelyaTick22 <odecohen@g.jct.ac.il>
2022-03-28 15:20:17 +0300

    Changing ThisIsTheWay.py

--- ThisIsTheWay.py
+++ ThisIsTheWay.py
@@ -1,11 +1,20 @@
 from os import listdir
 
 
-def way(path):
+def way(path: str) -> list[str]:
+    """
+    A function that gets a path to a folder,
+    and returns the list of all files that begin with the "deep" letter sequence in that folder
+    @param path: a path to a folder
+    @return list of all files that begin with the "deep" letter sequence in that folder
+    """
     files = []
     arr = listdir(path)
-    for name in arr:
-        if name.startswith("deep"):
-            files.append(name)
+    [files.append(name) for name in list(arr) if name.startswith("deep")]
     return files
 
+"""
+An example usage:
+print(way(r"C:\Users\IMOE001\Downloads"))
+>>['deep.txt', 'deepmkvgk.txt']
+"""
